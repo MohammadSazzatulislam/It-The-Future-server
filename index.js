cad626acde0ee7dbcc976121a939d15d8c0aca6b
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-10-24 23:13:32 +0600

    create product api

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT ||  5000;
 
+const allProducts = require('./data/Products.json')
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("All product Api is comming soon");
 });
 
+
+app.get('/products', (req, res) => {
+    res.send(allProducts);
+})
 
 
 app.listen(port, () => {
